c39295645d84b5e35e618e551634aeae1b3c4544
Antonis Kalipetis <akalipetis@gmail.com>
2012-12-05 17:01:37 +0200

    Used the adapter to populate the list view.

--- src/gr/ntua/eestec/eestec_android_workshop/MainActivity.java
+++ src/gr/ntua/eestec/eestec_android_workshop/MainActivity.java
@@ -1,5 +1,7 @@
 package gr.ntua.eestec.eestec_android_workshop;
 
+import java.util.ArrayList;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.ListView;
@@ -11,7 +13,17 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		ListView list = (ListView) findViewById(R.id.listView1);
+		ListView listView = (ListView) findViewById(R.id.listView1);
+
+		ArrayList<FlickrPhotoset> list = new ArrayList<FlickrPhotoset>();
+
+		for (int i = 0; i < 20; ++i) {
+
+			list.add(new FlickrPhotoset(i, "Hello, " + i));
+		}
+
+		FlickrPhotosetAdapter adapter = new FlickrPhotosetAdapter(list, this);
+		listView.setAdapter(adapter);
 	}
 
 }
